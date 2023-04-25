f2baf7e2d7cf5eabf7686da044d59d6f8d7c636d
Lorena <blorena1321@gmail.com>
2023-04-24 23:43:22 -0300

    refatorando 3

--- src/schemas/transacoes.schemas.js
+++ src/schemas/transacoes.schemas.js
@@ -2,6 +2,5 @@ import Joi from "joi";
 
 export const transacaoSchema = Joi.object({
     valor: Joi.number().positive().required(),
-    descricao: Joi.required(),
-    tipo: Joi.string().valid("entrada", "saida"),
+    descricao: Joi.required()
 })
